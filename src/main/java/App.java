469c5f3987b6a2c0ff68fad122183efd6315d2c1
Mickael Salvas <mickael_salvas21@hotmail.com>
2022-11-16 22:25:45 -0500

    Added source inspiration

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
+        // https://lisperator.net/pltut/parser/
         simpleAnalysis();
         mediumAnalysis();
         hardAnalysis();
